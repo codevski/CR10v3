91374d694672073c285563c0e767bbdc7e38dc05
codevski <1435321+codevski@users.noreply.github.com>
2021-12-20 11:28:00 +1100

    :memo: docs update

--- src/Version.h
+++ src/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.9.1-codevski-BL-250000"
+#define SHORT_BUILD_VERSION "2.0.9.2-codevski-BL"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2021-07-20"
+#define STRING_DISTRIBUTION_DATE "2021-12-20"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
